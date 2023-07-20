88c5cb4b281837f8a0e169cef55dfd6dcbcb43fc
kinsDev <kinsleygitonga@gmail.com>
2023-07-20 10:00:14 -0700

    reading user input using getline()

--- kinsley_practice/main.c
+++ kinsley_practice/main.c
@@ -3,7 +3,12 @@
 int main(int ac, char **argv)
 {
 	char *prompt = ":) >> ";
+	char *lineptr;
+	size_t n = 0;
 
 	printf("%s", prompt);
+	getline(&lineptr, &n, stdin);
+
+	free(lineptr);
 	return 0;
 }
