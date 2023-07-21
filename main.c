6b137e1d78c5c150ff80a7397a7810cde3248470
kinsDev <kinsleygitonga@gmail.com>
2023-07-21 01:40:40 -0700

    reading useer input and printing it out using the getline function

--- main.c
+++ main.c
@@ -3,8 +3,16 @@
 int main(int ac, char **argv)
 {
 	char *prompt = "kkshell :) ";
+	char *lineptr;
+	size_t n = 0l
+
+	(void)ac;
+	(void)argv;
 
         printf("%s", prompt);
+	getline(&lineptr, &n, stdin);
+	printf("%s\n", lineptr);
 
+	free(lineptr);
 	return 0;
 }
