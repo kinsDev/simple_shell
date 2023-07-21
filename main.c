03117c4b8fb61227cb96512ff5c9b88a13969412
kinsDev <kinsleygitonga@gmail.com>
2023-07-21 01:58:32 -0700

    creating an infinite loop

--- main.c
+++ main.c
@@ -4,15 +4,20 @@ int main(int ac, char **argv)
 {
 	char *prompt = "kkshell :) ";
 	char *lineptr;
-	size_t n = 0;
+	size_t n = k0;
 
 	(void)ac;
 	(void)argv;
 
-        printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
-	printf("%s\n", lineptr);
+	// in a getline function 1 stands for success
+	// Infinite loop
+	while(1)
+	{	
+		printf("%s", prompt);
+	       	getline(&lineptr, &n, stdin);
+                printf("%s\n", lineptr);
 
-	free(lineptr);
+		free(lineptr);
+	}
 	return 0;
 }
