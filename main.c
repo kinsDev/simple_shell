fbdb419235834c890ec12a53f11c4b93b5ab52aa
kinsDev <kinsleygitonga@gmail.com>
2023-07-21 23:07:49 -0700

    updating correct formats of commenting

--- main.c
+++ main.c
@@ -4,17 +4,17 @@ int main(int ac, char **argv)
 {
 	char *prompt = "kkshell :) ";
 	char *lineptr = NULL;
-	char *lineptr_cpy = NULL; // copy of the string to be passsed to strtok
+	char *lineptr_cpy = NULL; /* copy of the string to be passsed to strtok*/
 	size_t n = 0;
 	ssize_t line_read;
-	const char *delim = " \n"; // we'll be usin a space and new line character as our delimiter for now
+	const char *delim = " \n"; /* we'll be using a space and new line character as our delimiter for now */
 	int token_num = 0, i;
 	char *token;
 
 	(void)ac;
 
-	// in a getline function 1 stands for success
-	// Infinite loop
+	/* in a getline function 1 stands for success
+	   Infinite loop */
 	while(1)
 	{	
 		printf("%s", prompt);
@@ -23,12 +23,12 @@ int main(int ac, char **argv)
 		/* check if getline() reached EOF or user CTRL +D */
 		if(line_read == -1)
 		{
-			printf("\n"); // print new line before exiting the shell
+			printf("\n"); /* print new line before exiting the shell */
 			return -1;
 		}
 	}
 
-	//dyhamically allocating space
+	/* dyhamically allocating space */
 	lineptr_cpy = malloc(sizeof(char) * line_read);
         if (lineptr_cpy== NULL)
 	{
@@ -38,7 +38,7 @@ int main(int ac, char **argv)
 
 	strcpy(lineptr_cpy, lineptr);
 
-	//tokenization
+	/* tokenization */
 	token = strtok(lineptr, delim);
 	while(token != NULL)
 	{
@@ -47,10 +47,10 @@ int main(int ac, char **argv)
 	}
 	token_num++;
 
-	// space for string array
+	/* space for string array */
 	argv = malloc(sizeof(char *) * token_num);
 
-	// storing each token in an array
+	/* storing each token in an array */
 	token = strtok(lineptr_cpy, delim);
 
 	for(i = 0; token != NULL; i++)
@@ -63,7 +63,7 @@ int main(int ac, char **argv)
 	argv[i] = NULL;
 
 	printf("%s\n", lineptr);
-	// freeing up memeory that was previously allocated
+	/* freeing up memeory that was previously allocated */
 	free(lineptr);
 
 	return 0;
