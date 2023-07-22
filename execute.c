ccad0cd2a1fc49e9f089516e48c90e9b0de78300
kinsDev <kinsleygitonga@gmail.com>
2023-07-22 06:20:18 -0700

    execve command reading

--- execute.c
+++ execute.c
@@ -2,5 +2,17 @@
 
 void execmd(char **argv)
 {
+	char *command = NULL;
 
+	if(argv != NULL)
+	{
+		/* get the command keyed i by the end user */
+		command = argv[0];
+
+		/* executing the command that we got using execve */
+		if (execve(command, argv, NULL) == -1)
+		{
+			perror("Error: command not available");
+		}
+	}
 }
