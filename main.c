6ddf03b7deab0ddcdb7baf1cdd5e96ac04b48269
kinsDev <kinsleygitonga@gmail.com>
2023-07-21 22:04:13 -0700

    tokenization amd allocating memory dynamically

--- main.c
+++ main.c
@@ -4,8 +4,10 @@ int main(int ac, char **argv)
 {
 	char *prompt = "kkshell :) ";
 	char *lineptr = NULL;
+	char *lineptr_cpy = NULL; // copy of the string to be passsed to strtok
 	size_t n = 0;
 	ssize_t line_read;
+	const char *delim = " \n"; // we'll be usin a space and new line character as our delimiter for now
 
 	(void)ac;
 	(void)argv;
@@ -25,6 +27,26 @@ int main(int ac, char **argv)
 		}
                 printf("%s\n", lineptr);
 	}
+
+	//dyhamically allocating space
+	lineptr_cpy = malloc(sizeof(char) * line_read);
+        if (lineptr_cpy== NULL)
+	{
+	       	perror("Eroor: Memory allocation");
+                return -1;
+        }
+
+	strcpy(lineptr_cpy, lineptr);
+
+	//tokenization
+	token = strtok(lineptr, delim);
+	while(token != NULL)
+	{
+		token_num++;
+		token = strtok(NULL, delim);
+	}
+	token_num++;
+
 	free(lineptr);
 
 	return 0;
