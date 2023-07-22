55dd9b941a9e7bae47d140ab2c12b0797eab0f8a
kinsDev <kinsleygitonga@gmail.com>
2023-07-21 22:24:30 -0700

    updates on the main.c file

--- main.c
+++ main.c
@@ -8,9 +8,10 @@ int main(int ac, char **argv)
 	size_t n = 0;
 	ssize_t line_read;
 	const char *delim = " \n"; // we'll be usin a space and new line character as our delimiter for now
+	int token_num = 0, i;
+	char *token;
 
 	(void)ac;
-	(void)argv;
 
 	// in a getline function 1 stands for success
 	// Infinite loop
@@ -25,7 +26,6 @@ int main(int ac, char **argv)
 			printf("\n"); // print new line before exiting the shell
 			return -1;
 		}
-                printf("%s\n", lineptr);
 	}
 
 	//dyhamically allocating space
@@ -47,6 +47,23 @@ int main(int ac, char **argv)
 	}
 	token_num++;
 
+	// space for string array
+	argv = malloc(sizeof(char *) * token_num);
+
+	// storing each token in an array
+	token = strtok(lineptr_cpy, delim);
+
+	for(i = 0; token != NULL; i++)
+	{
+		argv[i] = malloc(sizeof(char) * strlen(token));
+		strcpy(argv[i], token);
+
+		token = strtok(NULL, delim);
+	}
+	argv[i] = NULL;
+
+	printf("%s\n", lineptr);
+	// freeing up memeory that was previously allocated
 	free(lineptr);
 
 	return 0;
