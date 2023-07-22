15ffbe1b68b08196be9b9b11a0a2abf6ad8a3e05
kinsDev <kinsleygitonga@gmail.com>
2023-07-22 06:35:24 -0700

    updated the main.h headers

--- main.h
+++ main.h
@@ -1,4 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+/* declaring the execmd function that takes in the execve in the execute file */
+void execmd(char **argv);
+
+#endif
