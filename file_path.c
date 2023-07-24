e4a5dca2b5a8ba535c676976e42f194f7df135af
kinsDev <kinsleygitonga@gmail.com>
2023-07-24 03:44:29 -0700

    getting the path environment variable

---
+++ file_path.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+char *get_location(char *command)
+{
+    char *path, *path_copy;
+
+    /* getenv function is used to get the path environment variable */
+    path = getenv("PATH");
+}
