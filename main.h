6ebaffe357cd73ee96b1e5fd03e4b9d9424496f1
kinsDev <kinsleygitonga@gmail.com>
2023-07-24 03:52:12 -0700

    header files for the stat function

--- main.h
+++ main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /* declaring the execmd function that takes in the execve in the execute file */
 void execmd(char **argv);
