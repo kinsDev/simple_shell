382caec3e991fffff6dbc9ced1ad9288cfd9140a
kinsDev <kinsleygitonga@gmail.com>
2023-07-25 02:09:49 -0700

    generating path to the command

--- execute.c
+++ execute.c
@@ -2,15 +2,18 @@
 
 void execmd(char **argv)
 {
-	char *command = NULL;
+	char *cmd = NULL, *typed_cmd = NULL;
 
 	if(argv != NULL)
 	{
 		/* get the command keyed i by the end user */
-		command = argv[0];
+		cmd = argv[0];
+
+		/* generating path to the keyed in command */
+		typed_cmd = get_location(cmd);
 
 		/* executing the command that we got using execve */
-		if (execve(command, argv, NULL) == -1)
+		if (execve(typed_cmd, argv, NULL) == -1)
 		{
 			perror("Error: command not available");
 		}
