b9028f0ae1d58d1cd391e2ec46f16fe38fcf28bf
peter <01peterkinyanjui@gmail.com>
2023-07-26 03:09:38 -0700

    Getting file

--- file_access.c
+++ file_access.c
@@ -13,6 +13,7 @@ char *move_left(char *a, int size, int num)
 {
 	int i;
 	char *ptr;
+
 	for (i = 0, ptr = (a + size - num); i < num; i++, ptr++)
 		*(a + i) = *ptr;
 	*(a + num) = '\0';
@@ -38,26 +39,26 @@ char *get_binary(unsigned int n)
 			ptr[0] = '0';
 			ptr[1] = '\0';
 		}
-	return (ptr);
+		return (ptr);
 	}
 	size = sizeof(int) * 8 + 1;
 	ptr = malloc(size);
 	if (ptr)
 	{
-	for (i = 0; i < size; i++)
-        	ptr[i] = '0';
+		for (i = 0; i < size; i++)
+			ptr[i] = '0';
 		ptr[size - 1] = '\0';
 
-	i = size - 2;
-	while (n != 0)
-	{
-		ptr[i] = n % 2 == 0 ? '0' : '1';
+		i = size - 2;
+		while (n != 0)
+		{
+			ptr[i] = n % 2 == 0 ? '0' : '1';
+			num++;
+			n = n / 2;
+			i--;
+		}
 		num++;
-		n = n / 2;
-		i--;
-	}
-	num++;
-	ptr = move_left(ptr, size, num);
+		ptr = move_left(ptr, size, num);
 	}
 	return (ptr);
 }
@@ -79,4 +80,89 @@ char *get_char(char c)
 	ptr[0] = c;
 	ptr[1] = '\0';
 	return (ptr);
+}
+
+/**
+ * get_string - Returns a duplicated string.
+ * @s: The string to be duplicated.
+ *
+ * Return: Pointer to the duplicated string or NULL if @s is NULL.
+ */
+char *get_string(char *s)
+{
+	char *ptr = NULL;
+	int i, j;
+
+	if (s == NULL)
+	{
+		ptr = malloc(7);
+		if (ptr == NULL)
+			return (NULL);
+
+		ptr[0] = '(';
+		ptr[1] = 'n';
+		ptr[2] = 'u';
+		ptr[3] = 'l';
+		ptr[4] = 'l';
+		ptr[5] = ')';
+		ptr[6] = '\0';
+		return (ptr);
+	}
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	ptr = (char *)malloc(i * sizeof(char) + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		ptr[j] = s[j];
+
+	ptr[j] = '\0';
+	return (ptr);
+}
+
+/**
+ * get_number - Converts an integer to a string and stores it in a memory block.
+ * @n: Integer to be converted.
+ *
+ * Return: Pointer to the string representation of the integer, or NULL on error.
+ */
+char *get_number(int n)
+{
+	int i, len = 0, tmp;
+	char *buf = NULL;
+
+	/* Find the number of bytes to allocate */
+	tmp = n;
+	while (tmp >= 10 || tmp <= -10)
+	{
+		tmp /= 10;
+		len++;
+	}
+	len++;
+	if (n < 0)
+		len++;
+
+	buf = malloc(sizeof(char) * (len + 1));
+	if (buf)
+	{
+		buf[len] = '\0';
+
+		if (n < 0)
+			buf[0] = '-';
+
+		i = len - 1;
+		while (n >= 10 || n <= -10)
+		{
+			tmp = (n % 10) >= 0 ? n % 10 : -(n % 10);
+			buf[i] = tmp + '0';
+			i--;
+			n /= 10;
+		}
+
+		tmp = (n % 10) >= 0 ? n % 10 : -(n % 10);
+		buf[i] = tmp + '0';
+	}
 }
