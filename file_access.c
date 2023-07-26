c20eabe7d560f15034f039b09111853370d0fb4f
peter <01peterkinyanjui@gmail.com>
2023-07-26 02:40:07 -0700

    Getting file

--- file_access.c
+++ file_access.c
@@ -1,48 +1,20 @@
-#include "main.h"
 #include <stdlib.h>
-#include <stdarg.h>
+#include "kkshell.h"
 
 /**
- * get_formatted_argument - Returns a formatted argument based on the type specifier.
+ * move_left - Removes leading zeros from a binary string.
+ * @a: Buffer containing the binary string.
+ * @size: Size of the buffer.
+ * @num: Number of digits in the binary number.
  *
- *
- * This function takes a type specifier and a variable number of arguments and
- * returns a dynamically allocated string representation of the argument based
- * on the type specifier.
- *
- * The supported type specifiers are:
- *   - 'c': Char argument is passed as int and returned as a string.
- *   - 's': String argument is passed as char* and returned as a string.
- *   - 'd' or 'i': Integer argument is passed as int and returned as a string.
- *   - 'b': Integer argument is passed as int and returned as a binary string.
- *   - 'r': String argument is passed as char* and returned as a reversed string.
- *   - 'R': String argument is passed as char* and returned as a ROT13 string.
- *
- * Return: A dynamically allocated string representation of the argument based
- *         on the type specifier,
- *         or NULL if an unsupported type specifier is provided.
+ * Return: Pointer to the modified buffer.
  */
-char *get_formatted_argument(char type, ...)
+char *move_left(char *a, int size, int num)
 {
-    va_list params;
-    va_start(params, type);
-
-    switch (type)
-    {
-    case 'c':
-        return get_char_argument(va_arg(params, int));
-    case 's':
-        return get_string_argument(va_arg(params, char *));
-    case 'd':
-    case 'i':
-        return get_number_argument(va_arg(params, int));
-    case 'b':
-        return get_binary_argument(va_arg(params, int));
-    case 'r':
-        return get_reversed_string(va_arg(params, char *));
-    case 'R':
-        return get_rot13_encoded_string(va_arg(params, char *));
-    default:
-        return NULL; // Error: Unsupported type specifier
-    }
+	int i;
+	char *ptr;
+	for (i = 0, ptr = (a + size - num); i < num; i++, ptr++)
+		*(a + i) = *ptr;
+	*(a + num) = '\0';
+	return (a);
 }
