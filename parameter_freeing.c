598973ec966ca570b69d5f2d927a3ec3b95ce8f2
peter <01peterkinyanjui@gmail.com>
2023-07-26 04:12:17 -0700

    Freeing the memory allocated for the param_t structure

--- parameter_freeing.c
+++ parameter_freeing.c
@@ -13,30 +13,30 @@
  */
 void free_params(param_t *params)
 {
-    unsigned int i;
+unsigned int i;
 
-    /* Free the input buffer if it's not NULL */
-    if (params->buffer)
-        free(params->buffer);
+/* Free the input buffer if it's not NULL */
+if (params->buffer)
+	free(params->buffer);
 
-    /* Free the nextCommand buffer if it's not NULL */
-    if (params->nextCommand)
-        free(params->nextCommand);
+/* Free the nextCommand buffer if it's not NULL */
+if (params->nextCommand)
+	free(params->nextCommand);
 
-    /* Free the linked list of environment variables */
-    free_list(params->env_head);
+/* Free the linked list of environment variables */
+free_list(params->env_head);
 
-    /* Free the linked list of aliases */
-    free_list(params->alias_head);
+/* Free the linked list of aliases */
+free_list(params->alias_head);
 
-    /* Free each argument in the args array */
-    for (i = 0; params->args[i]; i++)
-        free(params->args[i]);
+/* Free each argument in the args array */
+for (i = 0; params->args[i]; i++)
+	free(params->args[i]);
 
-    /* Free the args array */
-    free(params->args);
+/* Free the args array */
+free(params->args);
 
-    /* Free the param_t structure itself */
-    free(params);
+/* Free the param_t structure itself */
+free(params);
 }
 
