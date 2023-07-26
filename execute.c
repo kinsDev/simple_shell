f25ac115f6c1316960d4df3f203d99cdeadf266a
kinsDev <kinsleygitonga@gmail.com>
2023-07-26 13:16:39 -0700

    getline

--- execute.c
+++ execute.c
@@ -2,54 +2,44 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-#include "kkshell.h"
+#include "shell.h"
 #include "lists.h"
 #include "main.h"
 /**
- * run_command - Execute a command by searching for it in the PATH
- * and executing it.
- * @params: Pointer to the param_t structure containing shell variables.
+ * run_command - searches path dirs for command and execs
+ * @params: parameters
  */
 void run_command(param_t *params)
 {
-	/* Function pointers to built-in commands and external executable */
-	void (*builtin_cmd)(param_t *);
-	char *executable_file = NULL;
+	char *exeFile = NULL;
 	pid_t pid;
-	/* Check if the command is a built-in command */
-	builtin_cmd = get_builtin(params);
-	if (builtin_cmd)
+	void (*buildin)(param_t *);
+
+	buildin = get_builtin(params);
+	if (buildin)
 	{
-		builtin_cmd(params);
+		buildin(params);
 		return;
 	}
-
-	/* Find the full path of the executable file in the PATH directories */
-	executable_file = get_file(params);
-	if (!executable_file)
+	exeFile = get_file(params);
+	if (!exeFile)
 	{
-		/* Error: Command not found or invalid */
 		return;
 	}
-
-	/* Fork a new process to execute the command */
 	pid = fork();
 	if (pid < 0)
 	{
-		/* Error: Forking failed */
 		free_params(params);
 		exit(98);
 	}
 	else if (pid == 0)
 	{
-		/* Child process: Execute the command */
-		execve(executable_file, params->args, NULL);
+		execve(exeFile, params->args, NULL);
 	}
 	else
 	{
-		/* Parent process: Wait for the child process to complete */
 		wait(&params->status);
 		params->status = WEXITSTATUS(params->status);
-		free(executable_file);
+		free(exeFile);
 	}
 }
--- setenv.c
+++ setenv.c
@@ -1,59 +1,79 @@
 #include <stdlib.h>
-#include "kkshell.h"
+#include "shell.h"
 #include "main.h"
 #include "lists.h"
 
 /**
- * _setenv - Set or update an environment variable with the given name and value.
- * @params: Pointer to the shell parameters struct.
- */
-void set_env_variable(param_t *params)
+ * _setenv - function searches the environment list to find the
+ * environment variable name, and sets to the corresponding
+ * value string.
+ * @params: parameters
+*/
+void _setenv(param_t *params)
 {
-    char *tmp = NULL;
-    char *name = params->args[1];
-    char *value = params->args[2];
-    list_t *current = params->env_head;
+	char *tmp = NULL;
+	char *name = params->args[1], *value = params->args[2];
+	list_t *h = params->env_head;
 
-    if (params->tokCount != 3)
-    {
-        /* Invalid number of arguments */
-        params->status = 0;
-        return;
-    }
-
-    while (current)
-    {
-        if (_strcmp(name, current->str) == 0) /* Environment variable exists */
-        {
-            /* Update existing environment variable */
-            tmp = current->val;
-            free(tmp);
-            current->val = _strdup(value);
-            if (!current->val)
-            {
-                /* Memory allocation error */
-                write(STDERR_FILENO, "setenv: memory allocation error\n", 30);
-                exit(-1);
-            }
-            current->valLen = _strlen(value);
-            params->status = 0;
-            return;
-        }
-        current = current->next;
-    }
-
-    /* Environment variable does not exist, add it to the list */
-    params->env_head = add_node(&(params->env_head), name, value);
-    params->status = 0;
+	if (params->tokCount != 3)
+	{
+		params->status = 0;
+		return;
+	}
+	while (h)
+	{
+		if (_strcmp(name, h->str) == 0) /* env var exists */
+		{
+			tmp = h->val;
+			free(tmp);
+			h->val = _strdup(value);
+			if (!h->val)
+			{
+				write(STDERR_FILENO, "setenv malloc error\n", 18);
+				exit(-1);
+			}
+			h->valLen = _strlen(value);
+			params->status = 0;
+			return;
+		}
+		h = h->next;
+	}
+	/* env var DNE */
+	params->env_head = add_node(&(params->env_head), name, value);
+	params->status = 0;
 }
-
 /**
- * _unsetenv - Remove an environment variable with the given name from the list.
- * @params: Pointer to the shell parameters struct.
- */
-void unset_env_variable(param_t *params)
+ * _unsetenv - function searches the environment list to find the
+ * environment variable name and removes it.
+ * @params: parameters
+*/
+void _unsetenv(param_t *params)
 {
-    char *name = params->args[1];
-    list_t *prev = NULL;
-   
+	char *name = params->args[1];
+	list_t *prev = NULL, *h = params->env_head;
+
+	if (params->tokCount != 2)
+	{
+		params->status = 0;
+		return;
+	}
+	while (h)
+	{
+		if (_strcmp(name, h->str) == 0) /* env var exists */
+		{
+			if (h == params->env_head)
+				params->env_head = h->next;
+			else
+				prev->next = h->next;
+			free(h->str);
+			free(h->val);
+			free(h);
+			params->status = 0;
+			return;
+		}
+		prev = h;
+		h = h->next;
+	}
+	params->status = 0;
+}
 
