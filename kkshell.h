bb9c1a8f27c9e09619fddfdeb5e2f562872a812e
peter <01peterkinyanjui@gmail.com>
2023-07-26 13:31:38 -0700

    header file

--- kkshell.h
+++ kkshell.h
@@ -1,60 +1,101 @@
 #ifndef MY_SHELL_H
 #define MY_SHELL_H
-
+#define _GNU_SOURCE
 #include <stdio.h>
 #include "lists.h"
-
-/* Custom data structure to hold all shell variables needed */
+/**
+ * struct param_s - structure used to hold all shell variables needed
+ * @argv: command line argument from main function
+ * @buffer: input buffer
+ * @args: array of arguments
+ * @nextCommand: the next command to process
+ * @argsCap: num of arguments the args array can hold
+ * @lineCount: total line of input
+ * @tokCount: num of tokens in a line input
+ * @status: run command return status
+ * @env_head: singly linked list of environment vars
+ * @alias_head: singly linked list of aliases
+ * Description: This structures hold all variables that passed into
+ * other functions.
+ */
 typedef struct param_s
 {
-    char **argv;               /* Command line argument from main function */
-    char *buffer;              /* Input buffer */
-    char **args;               /* Array of arguments */
-    char *nextCommand;         /* The next command to process */
-    unsigned int argsCap;      /* Number of arguments the args array can hold */
-    unsigned int lineCount;    /* Total line of input */
-    unsigned int tokCount;     /* Number of tokens in a line input */
-    int status;                /* Return status of the run command */
-    list_t *env_head;          /* Singly linked list of environment vars */
-    list_t *alias_head;        /* Singly linked list of aliases */
+	char **argv;
+	char *buffer;
+	char **args;
+	char *nextCommand;
+	unsigned int argsCap;
+	unsigned int lineCount;
+	unsigned int tokCount;
+	int status;
+	list_t *env_head;
+	list_t *alias_head;
 } param_t;
 
-/* Custom data structure for built-in commands */
+/**
+ * struct op - a buildin function
+ * @name: buildin name
+ * @func: pointer to buildin function
+ */
 typedef struct op
 {
-    char *name;                /* Built-in name */
-    void (*func)(param_t *);   /* Pointer to built-in function */
+	char *name;
+	void (*func)(param_t *);
 } op_t;
 
-/* Function prototypes */
-int process_string(param_t *params);
-char *_strdup(char *str);
+int process_string(param_t *);
+
+char *_strdup(char *);
+
 char *_strcpy(char *dest, const char *src);
-int _getline(param_t *params);
-int _strcmp(char *s1, char *s2);
-int _strcmp_n(char *s1, char *s2, int n);
+
+int _getline(param_t *);
+
+int _strcmp(char *, char *);
+
+int _strcmp_n(char *, char *, int n);
+
 char *_strtok(char *str, char *delim, char **savePtr);
-void run_command(param_t *params);
+
+void run_command(param_t *);
+
 void *_realloc(char **ptr, unsigned int old_size, unsigned int new_size);
-void _printenv(param_t *params);
-void sigint_handler(int sig);
+
+void _printenv(param_t *);
+
+void sigint_handler(int);
+
 char *_getenv(char *name, param_t *params);
+
 void _setenv(param_t *params);
+
 void _unsetenv(param_t *params);
+
 char *get_file(param_t *params);
+
 void (*get_builtin(param_t *params))(param_t *);
+
 void _myExit(param_t *params);
+
 void _cd(param_t *params);
+
 void _alias(param_t *params);
+
 void set_alias(char *name, param_t *params);
+
 void make_alias(char *name, char *val, param_t *params);
+
 void print_alias(char *name, param_t *params);
+
 void print_all_aliases(param_t *params);
-void print_list_env(list_t *head);
-void print_list_alias(list_t *head);
+
+void print_list_env(list_t *);
+
+void print_list_alias(list_t *);
+
 void free_params(param_t *params);
+
 void write_error(param_t *params, char *msg);
+
 void _clear(param_t *params);
-
-#endif /* MY_SHELL_H */
-
+#endif
