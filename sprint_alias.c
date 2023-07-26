e3676d0ffffa21cc9c3c23242bbe9ef96ee07688
kinsDev <kinsleygitonga@gmail.com>
2023-07-26 13:42:25 -0700

    printinmg value of an alias if it exists

--- sprint_alias.c
+++ sprint_alias.c
@@ -1,27 +1,25 @@
 #include <stdlib.h>
-#include "kkshell.h"
+#include "shell.h"
 #include "main.h"
 #include "lists.h"
 
 /**
- * print_alias - Prints the value of an alias, if it exists.
- * @name: Name of the alias to print.
- * @params: Parameter struct containing the alias list.
- *
- * Description: This function searches the alias list stored in the parameter
- * struct `alias_head` with the given `name`. If the alias is found, its value
- * is printed to stdout. If the alias is not found, an error message is printed
- * to stderr.
- */
+ * print_alias - prints the value of an alias, if it exists
+ * @name: name of the alias to print
+ * @params: parameter struct containing the alias list
+ * This function searches the alias list stored in the parameter struct  alias
+ * with the given name. If the alias is found, its value is printed to stdout.
+ * if alias is not found, an error message is printed to stderr.
+*/
 void print_alias(char *name, param_t *params)
 {
-	unsigned int name_len = 0;
+	unsigned int len = 0;
 	list_t *ptr = params->alias_head;
 
-	name_len = _strlen(name);
+	len = _strlen(name);
 	while (ptr)
 	{
-		if (_strcmp_n(name, ptr->str, name_len - 1) == 0)
+		if (_strcmp_n(name, ptr->str, len - 1) == 0)
 			_printf("%s=\'%s\'\n", ptr->str, ptr->val);
 		ptr = ptr->next;
 	}
@@ -29,12 +27,12 @@ void print_alias(char *name, param_t *params)
 }
 
 /**
- * print_all_aliases - Prints all aliases in the alias list.
- * @params: Parameter struct containing the alias list.
+ * print_all_aliases - prints all aliases in the alias list
+ * @params: parameter struct containing the alias list
  *
- * Description: This function iterates through the alias list stored in the
- * parameter struct and prints the name and value of each alias to stdout.
- */
+ * This function iterates through the alias list stored in the parameter struct
+ * and prints the name and value of each alias to stdout.
+*/
 void print_all_aliases(param_t *params)
 {
 	print_list_alias(params->alias_head);
@@ -42,12 +40,12 @@ void print_all_aliases(param_t *params)
 }
 
 /**
- * print_list_alias - Prints the name and value of each alias in a linked list.
- * @head: Pointer to the head of the linked list.
+ * print_list_alias - prints the name and value of each alias in a linked list
+ * @head: pointer to the head of the linked list
  *
- * Description: This function recursively iterates through the linked list of
- * aliases and prints the name and value of each alias to stdout.
- */
+ * This function iterates through the linked list of aliases and print the name
+ * and value of each alias to stdout.
+*/
 void print_list_alias(list_t *head)
 {
 	if (head)
@@ -57,4 +55,3 @@ void print_list_alias(list_t *head)
 			_printf("%s=\'%s\'\n", head->str, head->val);
 	}
 }
-
