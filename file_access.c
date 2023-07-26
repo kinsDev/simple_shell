366ff58717b52bb844cc212a73b98f510b59989a
peter <01peterkinyanjui@gmail.com>
2023-07-26 03:02:54 -0700

    Getting file

--- file_access.c
+++ file_access.c
@@ -18,3 +18,65 @@ char *move_left(char *a, int size, int num)
 	*(a + num) = '\0';
 	return (a);
 }
+
+/**
+ * get_binary - Converts an unsigned int to a binary string.
+ * @n: Integer to be converted.
+ *
+ * Return: Pointer to the binary string.
+ */
+char *get_binary(unsigned int n)
+{
+	char *ptr = NULL;
+	unsigned int i, num = 0, size = 0;
+
+	if (n == 0)
+	{
+		ptr = malloc(2);
+		if (ptr)
+		{
+			ptr[0] = '0';
+			ptr[1] = '\0';
+		}
+	return (ptr);
+	}
+	size = sizeof(int) * 8 + 1;
+	ptr = malloc(size);
+	if (ptr)
+	{
+	for (i = 0; i < size; i++)
+        	ptr[i] = '0';
+		ptr[size - 1] = '\0';
+
+	i = size - 2;
+	while (n != 0)
+	{
+		ptr[i] = n % 2 == 0 ? '0' : '1';
+		num++;
+		n = n / 2;
+		i--;
+	}
+	num++;
+	ptr = move_left(ptr, size, num);
+	}
+	return (ptr);
+}
+
+/**
+ * get_char - Returns a copy of a single character.
+ * @c: The character to be copied.
+ *
+ * Return: Pointer to the copied character.
+ */
+char *get_char(char c)
+{
+	char *ptr = NULL;
+
+	ptr = malloc(2);
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr[0] = c;
+	ptr[1] = '\0';
+	return (ptr);
+}
