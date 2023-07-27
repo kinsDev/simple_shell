c29d4f48e453f2ab70aa2a8a0553fc48f747aba5
kinsDev <kinsleygitonga@gmail.com>
2023-07-26 17:39:47 -0700

    returning formatted arguments

--- argument_checker.c
+++ argument_checker.c
@@ -3,16 +3,17 @@
 #include <stdarg.h>
 
 /**
- * get_formatted_argument - Returns a formatted argument based on the type specifier.
+ * get_arg - Returns a formatted argument based on the type specifier.
  *
  * @type: Type specifier indicating the desired formatting ('c', 's', 'd', 'i',
  *         'b', 'r', 'R').
  * @...: Variable number of arguments depending on the type specifier.
  *
  * This function takes a type specifier and a variable number of arguments and
- * returns a dynamically allocated string representation of the argument based on the type specifier.
+ * returns a dynamically allocated string representation of the argument based
+ * on the type specifier.
  *
- *    The supported type specifiers are:
+ * The supported type specifiers are:
  *   - 'c': Char argument is passed as int and returned as a string.
  *   - 's': String argument is passed as char* and returned as a string.
  *   - 'd' or 'i': Integer argument is passed as int and returned as a string.
@@ -21,41 +22,30 @@
  *   - 'R': String argument is passed as char* and returned as a ROT13 string.
  *
  * Return: A dynamically allocated string representation of the argument based
- *        on the type specifier,
- *         or NULL if an unsupported type specifier is provided.
+ *        on the type specifier, or NULL if an unsupported type specifier is provided.
  */
-char *get_formatted_argument(char type, ...)
+char *get_arg(char type, ...)
 {
 	va_list params;
-	char *result = NULL;
-
+	
 	va_start(params, type);
 	switch (type)
 	{
-	case 'c':
-		result = get_char_argument(va_arg(params, int));
-		break;
-	case 's':
-		result = get_string_argument(va_arg(params, char*));
-		break;
-	case 'd':
-	case 'i':
-		result = get_number_argument(va_arg(params, int));
-		break;
-	case 'b':
-		result = get_binary_argument(va_arg(params, int));
-		break;
-	case 'r':
-		result = get_reversed_string(va_arg(params, char*));
-		break;
-	case 'R':
-		result = get_rot13_encoded_string(va_arg(params, char*));
-		break;
-	default:
-		result = NULL; /* Error: Unsupported type specifier */
+		case 'c':
+			return (get_char(va_arg(params, int)));
+		case 's':
+			return (get_string(va_arg(params, char*)));
+		case 'd':
+		case 'i':
+			return (get_number(va_arg(params, int)));
+		case 'b':
+			return (get_binary(va_arg(params, int)));
+		case 'r':
+			return (get_rev(va_arg(params, char *)));
+		case 'R':
+			return (get_rot13(va_arg(params, char *)));
+		default:
+			return (NULL); /* Error: Unsupported type specifier */
 	}
-
-	va_end(params);
-	return result;
 }
 
