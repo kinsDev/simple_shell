d813e465b6e9cf7f8fb9ed16670d3ac0ad422a3b
kinsDev <kinsleygitonga@gmail.com>
2023-07-26 17:25:48 -0700

    searching the environment list

--- setenv.c
+++ setenv.c
@@ -4,76 +4,85 @@
 #include "lists.h"
 
 /**
- * _setenv - function searches the environment list to find the
- * environment variable name, and sets to the corresponding
+ * _setenv - Searches the environment list to find the
+ * environment variable name, and sets it to the corresponding
  * value string.
- * @params: parameters
-*/
+ * @params: Pointer to a param_t struct with the shell state and arguments.
+ */
 void _setenv(param_t *params)
 {
-	char *tmp = NULL;
-	char *name = params->args[1], *value = params->args[2];
-	list_t *h = params->env_head;
+    char *env_name = NULL;
+    char *env_value = NULL;
+    char *name = params->args[1];
+    char *value = params->args[2];
+    list_t *current_env = params->env_head;
 
-	if (params->tokCount != 3)
-	{
-		params->status = 0;
-		return;
-	}
-	while (h)
-	{
-		if (_strcmp(name, h->str) == 0) /* env var exists */
-		{
-			tmp = h->val;
-			free(tmp);
-			h->val = _strdup(value);
-			if (!h->val)
-			{
-				write(STDERR_FILENO, "setenv malloc error\n", 18);
-				exit(-1);
-			}
-			h->valLen = _strlen(value);
-			params->status = 0;
-			return;
-		}
-		h = h->next;
-	}
-	/* env var DNE */
-	params->env_head = add_node(&(params->env_head), name, value);
-	params->status = 0;
+    if (params->tokCount != 3)
+    {
+        params->status = 0;
+        return;
+    }
+
+    while (current_env)
+    {
+        if (_strcmp(name, current_env->str) == 0) /* Environment variable exists */
+        {
+            env_name = current_env->val;
+            free(env_name);
+            current_env->val = _strdup(value);
+            if (!current_env->val)
+            {
+                write(STDERR_FILENO, "setenv malloc error\n", 18);
+                exit(-1);
+            }
+            current_env->valLen = _strlen(value);
+            params->status = 0;
+            return;
+        }
+        current_env = current_env->next;
+    }
+
+    /* Environment variable does not exist */
+    params->env_head = add_node(&(params->env_head), name, value);
+    params->status = 0;
 }
+
 /**
- * _unsetenv - function searches the environment list to find the
+ * _unsetenv - Searches the environment list to find the
  * environment variable name and removes it.
- * @params: parameters
-*/
+ * @params: Pointer to a param_t struct with the shell state and arguments.
+ */
 void _unsetenv(param_t *params)
 {
-	char *name = params->args[1];
-	list_t *prev = NULL, *h = params->env_head;
+    char *name = params->args[1];
+    list_t *prev_env = NULL;
+    list_t *current_env = params->env_head;
 
-	if (params->tokCount != 2)
-	{
-		params->status = 0;
-		return;
-	}
-	while (h)
-	{
-		if (_strcmp(name, h->str) == 0) /* env var exists */
-		{
-			if (h == params->env_head)
-				params->env_head = h->next;
-			else
-				prev->next = h->next;
-			free(h->str);
-			free(h->val);
-			free(h);
-			params->status = 0;
-			return;
-		}
-		prev = h;
-		h = h->next;
-	}
-	params->status = 0;
+    if (params->tokCount != 2)
+    {
+        params->status = 0;
+        return;
+    }
+
+    while (current_env)
+    {
+        if (_strcmp(name, current_env->str) == 0) /* Environment variable exists */
+        {
+            if (current_env == params->env_head)
+                params->env_head = current_env->next;
+            else
+                prev_env->next = current_env->next;
+
+            free(current_env->str);
+            free(current_env->val);
+            free(current_env);
+            params->status = 0;
+            return;
+        }
+        prev_env = current_env;
+        current_env = current_env->next;
+    }
+
+    params->status = 0;
 }
 
