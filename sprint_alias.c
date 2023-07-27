985a929d8e0447601f2fcc0cbc5188a5ccbdaa88
peter <01peterkinyanjui@gmail.com>
2023-07-26 18:28:33 -0700

    sprint aliases

--- sprint_alias.c
+++ sprint_alias.c
@@ -3,55 +3,38 @@
 #include "main.h"
 #include "lists.h"
 
-/**
- * print_alias - prints the value of an alias, if it exists
- * @name: name of the alias to print
- * @params: parameter struct containing the alias list
- * This function searches the alias list stored in the parameter struct  alias
- * with the given name. If the alias is found, its value is printed to stdout.
- * if alias is not found, an error message is printed to stderr.
-*/
+/* Function to print the value of a specific alias if it exists */
 void print_alias(char *name, param_t *params)
 {
-	unsigned int len = 0;
-	list_t *ptr = params->alias_head;
+    unsigned int len = 0;
+    list_t *ptr = params->alias_head;
 
-	len = _strlen(name);
-	while (ptr)
-	{
-		if (_strcmp_n(name, ptr->str, len - 1) == 0)
-			_printf("%s=\'%s\'\n", ptr->str, ptr->val);
-		ptr = ptr->next;
-	}
-	params->status = 0;
+    len = _strlen(name);
+    while (ptr)
+    {
+        if (_strcmp_n(name, ptr->str, len - 1) == 0)
+            _printf("%s=\'%s\'\n", ptr->str, ptr->val);
+        ptr = ptr->next;
+    }
+    params->status = 0;
 }
 
-/**
- * print_all_aliases - prints all aliases in the alias list
- * @params: parameter struct containing the alias list
- *
- * This function iterates through the alias list stored in the parameter struct
- * and prints the name and value of each alias to stdout.
-*/
+/* Function to print all aliases in the alias list */
 void print_all_aliases(param_t *params)
 {
-	print_list_alias(params->alias_head);
-	params->status = 0;
+    /* Call helper function to print the linked list of aliases */
+    print_list_alias(params->alias_head);
+    params->status = 0;
 }
 
-/**
- * print_list_alias - prints the name and value of each alias in a linked list
- * @head: pointer to the head of the linked list
- *
- * This function iterates through the linked list of aliases and print the name
- * and value of each alias to stdout.
-*/
+/* Helper function to print the name and value of each alias in a linked list */
 void print_list_alias(list_t *head)
 {
-	if (head)
-	{
-		print_list_alias(head->next);
-		if (head->str != NULL)
-			_printf("%s=\'%s\'\n", head->str, head->val);
-	}
+    if (head)
+    {
+        /* Recursively traverse the linked list to print aliases in reverse order */
+        print_list_alias(head->next);
+        if (head->str != NULL)
+            _printf("%s=\'%s\'\n", head->str, head->val);
+    }
 }
