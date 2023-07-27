9360b5cdbb0dcb8bcd5545132a3f1e849b2df9b0
kinsDev <kinsleygitonga@gmail.com>
2023-07-26 18:03:19 -0700

    handle and terminate signals

--- signals.c
+++ signals.c
@@ -1,10 +1,15 @@
 #include <unistd.h>
+
 /**
- * sigint_handler - handle terminate signal
- * @s: signal number
+ * sigint_handler - Signal handler for SIGINT (Ctrl+C) signal.
+ * @s: The signal number (unused attribute indicates that the parameter is not used).
+ *
+ * This function is called when the shell receives the SIGINT signal, typically
+ * when the user presses Ctrl+C. It writes a newline and the shell prompt ($)
+ * to the standard output to provide a new line for user input.
  */
 void sigint_handler(int __attribute__((unused)) s)
 {
-	write(1, "\n($) ", 5);
+    write(1, "\n($) ", 5);
 }
 
