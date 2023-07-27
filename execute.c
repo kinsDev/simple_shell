3ce81430c41fcb0f8cb22179d18295f4f58fcc57
kinsDev <kinsleygitonga@gmail.com>
2023-07-26 17:04:24 -0700

    executing commands

--- execute.c
+++ execute.c
@@ -17,10 +17,10 @@
  */
 void run_command(param_t *params)
 {
-    char *exeFile = NULL; // Stores the path to the executable file.
-    pid_t pid; // Stores the process ID of the child process.
+    char *exeFile = NULL; /* Stores the path to the executable file. */
+    pid_t pid; /* Stores the process ID of the child process. */
 
-    // Check if the command is a built-in command and execute it if it is.
+    /* Check if the command is a built-in command and execute it if it is. */
     void (*builtin_func)(param_t *);
     builtin_func = get_builtin(params);
     if (builtin_func)
@@ -29,35 +29,35 @@ void run_command(param_t *params)
         return;
     }
 
-    // Search for the executable file in the path directories.
+    /* Search for the executable file in the path directories. */
     exeFile = get_file(params);
     if (!exeFile)
     {
-        // If the executable file is not found, return.
+        /* If the executable file is not found, return. */
         return;
     }
 
-    // Fork a child process to execute the external command.
+    /* Fork a child process to execute the external command. */
     pid = fork();
     if (pid < 0)
     {
-        // Fork failed. Free resources and exit the child process.
+        /* Fork failed. Free resources and exit the child process. */
         free_params(params);
         exit(98);
     }
     else if (pid == 0)
     {
-        // Child process: execute the external command using execve.
+        /* Child process: execute the external command using execve. */
         execve(exeFile, params->args, NULL);
 
-        // If execve returns, it means the command execution failed.
-        // Print an error message and exit the child process.
+        /* If execve returns, it means the command execution failed. */
+        /* Print an error message and exit the child process. */
         _printf("kkshell: command not found: %s\n", params->args[0]);
         exit(127);
     }
     else
     {
-        // Parent process: wait for the child process to complete.
+        /* Parent process: wait for the child process to complete. */
         wait(&params->status);
         params->status = WEXITSTATUS(params->status);
         free(exeFile);
