5bd0e1f2dfd3e738413cfa4b2e56974a0ac8e195
kinsDev <kinsleygitonga@gmail.com>
2023-07-26 18:49:39 -0700

    free memory allocated on parameters

--- parameter_freeing.c
+++ parameter_freeing.c
@@ -5,36 +5,38 @@
 /**
  * free_params - Frees the memory allocated for the param_t structure.
  * 
- * @params: pointer to the param_t structure to free
+ * @params: Pointer to the param_t structure to free.
  *
  * This function frees the dynamically allocated memory used by the param_t
  * structure, including its members: the array of arguments, the environment
- * list, and the input buffer.
+ * list, the alias list, the input buffer, and the nextCommand string.
  */
-
 void free_params(param_t *params)
 {
-	/* Free the input buffer if it was allocated */
-	if (params->buffer)
-		free(params->buffer);
+    /* Free the input buffer */
+    if (params->buffer)
+        free(params->buffer);
 
-	/* Free the nextCommand string if it was allocated */
-	if (params->nextCommand)
-		free(params->nextCommand);
+    /* Free the next command string */
+    if (params->nextCommand)
+        free(params->nextCommand);
 
-	/* Free the environment list */
-	free_list(params->env_head);
+    /* Free the environment list */
+    free_list(params->env_head);
 
-	/* Free the alias list */
-	free_list(params->alias_head);
+    /* Free the alias list */
+    free_list(params->alias_head);
 
-	/* Free each argument in the arguments array and then free the array itself */
-	for (unsigned int i = 0; params->args[i]; i++) {
-		free(params->args[i]);
-	}
-	free(params->args);
+    /* Free each argument in the args array */
+    for (unsigned int i = 0; params->args[i]; i++)
+    {
+        free(params->args[i]);
+    }
 
-	/* Free the param_t structure itself */
-	free(params);
+    /* Free the args array itself */
+    free(params->args);
+
+    /* Free the param_t structure itself */
+    free(params);
 }
 
